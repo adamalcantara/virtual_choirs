6e71c5e397f24891f875e6f660288dd1d9718051
adamalcantara <alcantaradevelopment@gmail.com>
2021-09-09 12:24:33 -0400

    Added and styled hero section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,8 +3,13 @@ import "./Home.css"
 
 const Home = () => {
     return (
-        <div>
-            <div id="hero" className="section"></div>
+        <div id="home">
+            <div id="hero" className="section">
+                <div id="text">
+                <h2>We're very happy you're here.</h2>
+                <h2>Please take a look at our work below.</h2>
+                </div>
+            </div>
             <div id="about" className="section"></div>
             <div id="media" className="section"></div>
             {/* <div id="medialink" className="section"></div> */}
