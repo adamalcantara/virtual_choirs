568e609d1676ec1c8495dd3fdb84402acca45187
adamalcantara <alcantaradevelopment@gmail.com>
2021-09-09 10:52:48 -0400

    Added state and handleclick function

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -7,6 +7,12 @@ import Logo from "../../Assets/img/logowhite.png";
 import "./Navbar.css"
 
 class Navbar extends Component {
+    state = { clicked: false }
+
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+    
     render(){
         return (
             <div id="navigation">
